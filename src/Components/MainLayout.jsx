438255073cf271f447dce29145d420d81af42746
Rizbi ahmmad <contact.rizbi123@gmail.com>
2024-12-05 01:25:58 +0600

    Setup Routes

---
+++ src/Components/MainLayout.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+const MainLayout = () => {
+    return (
+        <div>
+            <h2>This is main layout</h2>
+            <Navbar></Navbar>
+        </div>
+    );
+};
+
+export default MainLayout;
