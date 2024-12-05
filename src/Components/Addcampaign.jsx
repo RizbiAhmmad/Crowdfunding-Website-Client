c95c3ab68036cee491f11941b801fd73393479bf
Rizbi ahmmad <contact.rizbi123@gmail.com>
2024-12-06 01:01:45 +0600

    Adding campaign in database with swal

--- src/Components/Addcampaign.jsx
+++ src/Components/Addcampaign.jsx
@@ -1,26 +1,59 @@
 import React from 'react';
+import Swal from 'sweetalert2'
 
 const Addcampaign = () => {
+
+  const handleAddCampaign = event => {
+    event.preventDefault();
+    console.log('Form submitted');
+
+    const form = event.target;
+
+    const title=form.title.value;
+    const type=form.type.value;
+    const description=form.description.value;
+    const minDonation=form.minDonation.value;
+    const deadline=form.deadline.value;
+    const userEmail=form.userEmail.value;
+    const userName=form.userName.value;
+    const image=form.image.value;
+
+    const newCampaign={title,type,description,minDonation,deadline,userEmail,userName,image};
+
+    // Send data to the server
+    console.log(newCampaign);
+    fetch('http://localhost:5000/campaign',{
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newCampaign)
+    })
+    .then (res => res.json())
+    .then (data=>{
+      console.log(data);
+      if(data.insertedId){
+        Swal.fire({
+          title: 'Success!',
+          text: 'Campaign Added Successfully',
+          icon: 'success',
+          confirmButtonText: 'Close'
+        })
+      }
+    })
+
+  };
+
     return (
         <div className="container mx-auto px-4 py-8">
       <h2 className="text-3xl font-bold text-center text-teal-500 mb-6">
         Add New Campaign
       </h2>
-      <form
+      <form onSubmit={handleAddCampaign}
        
         className="max-w-lg mx-auto bg-white p-6 rounded-lg shadow-lg space-y-4"
       >
-        <div>
-          <label className="block font-medium mb-1">Image URL</label>
-          <input
-            type="url"
-            name="image"
-            
-            placeholder="Enter image URL"
-            className="input input-bordered w-full"
-            required
-          />
-        </div>
+       
         <div>
           <label className="block font-medium mb-1">Campaign Title</label>
           <input
@@ -78,19 +111,32 @@ const Addcampaign = () => {
           />
         </div>
         <div>
-          <label className="block font-medium mb-1">User Email</label>
+          <label className="block font-medium mb-1">User Name</label>
           <input
-            type="email"
-           
+            type="text"
+            name="userName"
+           placeholder="User Name"
             className="input input-bordered w-full bg-gray-100"
           />
         </div>
         <div>
-          <label className="block font-medium mb-1">User Name</label>
+          <label className="block font-medium mb-1">User Email</label>
           <input
-            type="text"
-           
+            type="email" placeholder="email@example.com"
+           name="userEmail"
             className="input input-bordered w-full bg-gray-100"
+          />
+        </div>
+        
+        <div>
+          <label className="block font-medium mb-1">Image URL</label>
+          <input
+            type="url"
+            name="image"
+            
+            placeholder="Enter image URL"
+            className="input input-bordered w-full"
+            required
           />
         </div>
         <button
